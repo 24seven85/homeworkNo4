64da0bed59f9e7e17268ed20075f12e28dad6a7f
maksimkjaulakis <24seven@live.ru>
2020-12-24 10:30:16 +0200

    add descendant classes

--- homeworkNo4/main.swift
+++ homeworkNo4/main.swift
@@ -10,19 +10,45 @@ import Foundation
 enum Color {
     case green, red, black
 }
+enum Clierence {
+    case low, high
+}
 
 class Car {
+    var brand: String
     let color: Color
     let wheels: Int
     let km: Int
     
     
-    init(color: Color, wheels: Int, km: Int) {
+    init(brand: String, color: Color, wheels: Int, km: Int) {
+        self.brand = brand
         self.color = color
         self.wheels = wheels
         self.km = km
     }
     func printCar() {
-        print(color, wheels, km)
+        
+    }
+}
+
+    
+class TrunkCar: Car {
+    let capacity: Int
+    let clierence: Clierence
+    init(brand: String, color: Color, wheels: Int, km: Int, capacity: Int, clierence: Clierence){
+        self.capacity = capacity
+        self.clierence = clierence
+        super.init(brand: brand, color: color, wheels: wheels, km: km)
+    }
+}
+
+class SportCar: Car {
+    let speed: Int
+    let clierence: Clierence
+    init(brand: String, color: Color, wheels: Int, km: Int, speed: Int, clierence: Clierence) {
+        self.speed = speed
+        self.clierence = clierence
+        super.init(brand: brand, color: color, wheels: wheels, km: km)
     }
 }
